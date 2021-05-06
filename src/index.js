7bfb71ec4bef7a3dbbd931c1684ae0a79d6249ec
Dmitrii Marianchenko <dima.marjan@gmail.com>
2021-05-06 12:26:27 +0300

    add cars

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import dishCardTpl from './templates/dish-cards.hbs';
+import dishesItems from './menu.json';
 import './styles.css';
+
+const menuItems = document.querySelector('ul.js-menu');
+menuItems.insertAdjacentHTML('beforeend', dishCardTpl(dishesItems));
